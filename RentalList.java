57eb0ff433bc0f857597d4b76cc04ef4764a8a3b
Daniella <166172006+daniraba@users.noreply.github.com>
2024-12-10 18:46:26 -0500

    Update RentalList.java

--- RentalList.java
+++ RentalList.java
@@ -1,3 +1,26 @@
 public class RentalList {
-    
+    private Customer customer;
+    private Movie movie; // Reference to a Movie object
+    private String rentalDate;
+    private String returnDate;
+
+    public RentalList(Customer customer, Movie movie, String rentalDate, String returnDate) {
+        this.customer = customer;
+        this.movie = movie;
+        this.rentalDate = rentalDate;
+        this.returnDate = returnDate;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    @Override
+    public String toString() {
+        return customer.getFirstName() + " rented " + movie.getTitle() + " from " + rentalDate + " to " + returnDate;
+    }
 }
